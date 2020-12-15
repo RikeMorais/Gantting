5d5f04c969664e950eb55cb08c365d6517fc32c7
Rike <rikeaju@hotmail.com>
2020-12-15 17:44:59 -0300

    Modificação do Schema

--- backend/src/api/tarefa/tarefa.js
+++ backend/src/api/tarefa/tarefa.js
@@ -1,7 +1,7 @@
 const restful = require('node-restful');
 const mongoose = restful.mongoose;
 
-const createSchema = new mongoose.Schema({
+const tarefa = new mongoose.Schema({
     name: {type: String, required: true},
     description: {type: String, required: false},
     prioridade: {type: String, required: false, uppercase: true, 
@@ -16,7 +16,7 @@ const cycleSchema = new mongoose.Schema({
     name: {type: String, required: true},
     month: {type: Number, min: 1, max: 12, required: true},
     year: {type: Number, min: 1970, max: 2100, required: true},
-    task: [createSchema]
+    task: [tarefa]
 });
 
 module.exports = restful.model('cycle', cycleSchema);
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 module.exports = mongoose.connect('mongodb://localhost/gantting');
+
+mongoose.Error.messages.general.required = "O atributo '{PATH} é obrigatório!' "
