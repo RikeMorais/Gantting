036c6a163ed39f0f158ac8969a33ce863b16e78d
Rike <rikeaju@hotmail.com>
2020-12-15 23:53:35 -0300

    Tratamento de Erros

--- backend/src/api/tarefa/tarefa.js
+++ backend/src/api/tarefa/tarefa.js
@@ -4,19 +4,14 @@ const mongoose = restful.mongoose;
 const tarefa = new mongoose.Schema({
     name: {type: String, required: true},
     description: {type: String, required: false},
-    prioridade: {type: String, required: false, uppercase: true, 
-        enum: ['URGENTE', 'ALTA', 'MÉDIA', 'BAIXA']},
-    dtini: {type: Date, required: true},
-    dtfim: {type: Date, required: false},
-    assignee: {type: String, required: true},
-    situation: {type: Boolean, required: false}    
+    assignee: {type: String, required: true},   
 });
 
 const cycleSchema = new mongoose.Schema({
     name: {type: String, required: true},
     month: {type: Number, min: 1, max: 12, required: true},
     year: {type: Number, min: 1970, max: 2100, required: true},
-    task: [tarefa]
+    tarefa: [tarefa]
 });
 
 module.exports = restful.model('cycle', cycleSchema);
--- backend/src/api/tarefa/tarefaService.js
+++ backend/src/api/tarefa/tarefaService.js
@@ -3,6 +3,16 @@ const tarefaService = require('./tarefa');
 tarefaService.methods(['get', 'post', 'put', 'delete']);
 tarefaService.updateOptions({new: true, runValidators: true});
 
+tarefaService.route('count', (req, res, next) =>{
+    tarefaService.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
+
 tarefaService.route('get', (req, res, next) => {
 
     tarefaService.find({}, (err, docs) => {
